d9fb3dadb4aba420eb43845f38761c2c3907c12c
Carl Bordum Hansen <carl@bordum.dk>
2017-09-16 15:16:30 +0200

    removed junk

--- manly.py
+++ manly.py
@@ -3,11 +3,6 @@ import subprocess
 import re
 
 
-HEADLINE_PATTERN = r'(?<=^NAME\n\s{7}).+'
-FLAG_HEADLINE_PATTERN = r'^\s+%s.+.+'
-FLAG_SECTION_PATTERN = r'^%s\n(^\s+.+\n+)+'
-
-
 def count_spaces(s):
     """Return the amount of spaces at the start of *s*."""
     for count, char in enumerate(s):
@@ -49,36 +44,6 @@ def get_flags(manpage):
     return flags
 
 
-def format_flag_repr(headline, description):
-    description[0] = description[0].capitalize()
-    if description[-1][-1] != '.':
-        description[-1] += '.'
-    indented = [' '*8 + line for line in description]
-    return '%s\n%s' % (
-            headline.strip(),
-            '\n'.join(indented),
-    )
-
-
-def get_flag_reprs(flags, option_section):
-    flag_reprs = []
-    for flag in flags:
-        description = []
-        match = re.search(FLAG_HEADLINE_PATTERN % flag,
-                          option_section, re.MULTILINE)
-        flag_headline = match.group(0)[1:]
-        indentation = count_spaces(flag_headline)
-        for line in option_section[match.end(0):].splitlines():
-            if line == '':
-                continue
-            if count_spaces(line) == indentation:
-                break
-            description.append(line.strip())
-
-        flag_reprs.append(format_flag_repr(flag_headline, description))
-    return flag_reprs
-
-
 def parse_flags(raw_flags):
     """Return a list of the flags in *raw_flags*."""
     flags = []
@@ -91,19 +56,24 @@ def parse_flags(raw_flags):
     return flags
 
 
+def format_headline(headline):
+    return headline
+
+
+def format_description(description):
+    return description
+
+
 def main():
     command = sys.argv[1]
     flags = parse_flags(sys.argv[2:])
 
     manpage = subprocess.check_output(['man', command]).decode('utf-8')
-    headline = re.search(HEADLINE_PATTERN, manpage, re.MULTILINE).group(0)
-    option_section = get_options_section(manpage)
-    flag_reprs = get_flag_reprs(flags, option_section)
+    flags = get_flags(manpage)
 
-    print(headline)
-    print('-' * len(headline))
-    for flag_repr in flag_reprs:
-        print(flag_repr, end='\n\n')
+    for headline, description in flags:
+        print(format_headline(headline))
+        print(format_description(description))
 
 
 if __name__ == '__main__':
